c92f9df7cb776b762c9d0c82e3b6086cb1289d9b
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-16 01:08:38 -0500

    Added the backward pass implementation for FC layer.

--- src/operator/cu_mlp_att_nonlin_block-inl.cuh
+++ src/operator/cu_mlp_att_nonlin_block-inl.cuh
@@ -26,19 +26,49 @@ static __global__ void _cuda_fused_mlp_att_nonlin_block__forward(
 	const RealType * const __restrict__ qry_hidden,
 	      RealType * const __restrict__ att_hidden, const bool layer_norm);
 
-// FullyConnected Layer Y = XW^T  Forward Pass
-// @param1 X [batch_size x input_dim]: (Input) Input Variable `X`
-// @param2 W [num_hidden x input_dim]: (Input) Weight Parameter `W`
-// @param3 O [batch_size x num_hidden]: (Output) Output Variable `Y`
+// FullyConnected Layer Y = XW^T Forward Pass
+// @param1 X [batch_size x input_dim] :  (Input) Input  Variable  `X`
+// @param2 W [num_hidden x input_dim] :  (Input) Weight Parameter `W`
+// @param3 Y [batch_size x num_hidden]: (Output) Output Variable  `Y`
 // @param4 batch_size: (Parameter) Batch Size
 // @param5 input_dim : (Parameter) Input Dimension
 // @param6 num_hidden: (Parameter) Number of Hidden Units
 template < typename RealType >
-static inline void FullyConnected(cublasHandle_t cublas_handle,
+static inline void FullyConnectedFW(cublasHandle_t cublas_handle,
 	const RealType * const __restrict__ X,
 	const RealType * const __restrict__ W,
-	      RealType * const __restrict__ O,
+	      RealType * const __restrict__ Y,
 	const unsigned batch_size, const unsigned input_dim, const unsigned num_hidden);
+
+// FullyConnected Layer Y = XW^T Backward Pass on Weight (dW = dY^T X)
+// @param1 dY [batch_size x num_hidden]:  (Input) Output Gradient `dY`
+// @param2  X [batch_size x input_dim] :  (Input)  Input Variable  `X`
+// @param3 dW [num_hidden x input_dim] : (Output) Weight Parameter Gradient `dW`
+// @param4 batch_size: (Parameter) Batch Size
+// @param5 input_dim : (Parameter) Input Dimension
+// @param6 num_hidden: (Parameter) Number of Hidden Units
+template < typename RealType >
+static inline void FullyConnectedBWWeight(cublasHandle_t cublas_handle,
+	const RealType * const __restrict__ dY,
+	const RealType * const __restrict__  X,
+	      RealType * const __restrict__ dW,
+	const OpReqType grad_req, const unsigned batch_size, 
+	const unsigned input_dim, const unsigned num_hidden);
+
+// FullyConnected Layer Y = XW^T Backward Pass on Data (dX = dY W)
+// @param1 dY [batch_size x num_hidden]:  (Input) Output Gradient `dY`
+// @param2  W [num_hidden x input_dim] :  (Input) Weight Parameter `W`
+// @param3 dX [batch_size x input_dim] : (Output)  Input Gradient `dX`
+// @param4 batch_size: (Parameter) Batch Size
+// @param5 input_dim : (Parameter) Input Dimension
+// @param6 num_hidden: (Parameter) Number of Hidden Units
+template < typename RealType >
+static inline void FullyConnectedBWData  (cublasHandle_t cublas_handle,
+	const RealType * const __restrict__ dY,
+	const RealType * const __restrict__  W,
+	      RealType * const __restrict__ dX,
+	const OpReqType grad_req, const unsigned batch_size, 
+	const unsigned input_dim, const unsigned num_hidden);
 
 template < typename DType >
 class CUMlpAttNonLinBlockOp : public Operator
@@ -150,12 +180,12 @@ public:
 		CHECK_EQ(cuda_stream->blas_handle_ownership_, Stream < gpu > ::OwnHandle) << 
 			"Must initialize the cuBLAS handle in CUDA stream.";
 		
-		FullyConnected(Stream < gpu > ::GetBlasHandle(cuda_stream),
-		               att_hidden.dptr_,
-		               h2s_weight.dptr_,
-			       att_scores.dptr_,
-			       _param.batch_size * _param.seq_length, 
-			       _param.state_size, 1);
+		FullyConnectedFW(Stream < gpu > ::GetBlasHandle(cuda_stream),
+		                 att_hidden.dptr_,
+		                 h2s_weight.dptr_,
+			         att_scores.dptr_,
+			         _param.batch_size * _param.seq_length, 
+			         _param.state_size, 1);
 	}
 
 	virtual void Backward(const OpContext & ctx,
@@ -176,7 +206,6 @@ public:
 		CHECK_EQ(out_data.size(), out_expected);
 		CHECK_EQ(out_grad.size(), out_expected);
 
-		// TODO: Double check that the gradient computations and make sure that they are or are NOT accumulative.
 		CHECK_NE(req[int(EnumOpInputs::SrcHidden)], kAddTo) << "AddTo is not supported for " "source hidden state.";
 		CHECK_NE(req[int(EnumOpInputs::QryHidden)], kAddTo) << "AddTo is not supported for "  "query hidden state.";
 		CHECK_NE(req[int(EnumOpInputs::H2SWeight)], kAddTo) << "AddTo is not supported for " "hidden-to-score weight.";
@@ -333,41 +362,121 @@ __global__ void _cuda_fused_mlp_att_nonlin_block__forward(
 }
 
 template <>
-inline void FullyConnected < float  > (cublasHandle_t cublas_handle,
-	const float  * const __restrict__ X,
-	const float  * const __restrict__ W,
-	      float  * const __restrict__ O,
+inline void FullyConnectedFW < float > (cublasHandle_t cublas_handle,
+	const float * const __restrict__ X,
+	const float * const __restrict__ W,
+	      float * const __restrict__ Y,
 	const unsigned batch_size, const unsigned input_dim, const unsigned num_hidden)
 {
-	float  alpha = 1.0, beta = 0.0;
+	float alpha = 1.0, beta = 0.0;
 
-	CUBLAS_CALL(cublasSgemm(cublas_handle, // cublas Handle
+	CUBLAS_CALL(cublasSgemm(cublas_handle, // cuBLAS Handle
 	                        CUBLAS_OP_T, // W.T
 				CUBLAS_OP_N, // X
 				num_hidden,  // Y.shape[1]
 				batch_size,  // Y.shape[0]
 				input_dim,   // W.shape[1]
 				&alpha, W, input_dim, X, input_dim,
-				& beta, O, num_hidden));
+				& beta, Y, num_hidden));
 }
 
 template <>
-inline void FullyConnected < double > (cublasHandle_t cublas_handle,
+inline void FullyConnectedFW < double > (cublasHandle_t cublas_handle,
 	const double * const __restrict__ X,
 	const double * const __restrict__ W,
-	      double * const __restrict__ O,
+	      double * const __restrict__ Y,
 	const unsigned batch_size, const unsigned input_dim, const unsigned num_hidden)
 {
 	double alpha = 1.0, beta = 0.0;
 
-	CUBLAS_CALL(cublasDgemm(cublas_handle, // cublas Handle
+	CUBLAS_CALL(cublasDgemm(cublas_handle, // cuBLAS Handle
 	                        CUBLAS_OP_T, // W.T
 				CUBLAS_OP_N, // X
 				num_hidden,  // Y.shape[1]
 				batch_size,  // Y.shape[0]
 				input_dim,   // W.shape[1]
 				&alpha, W, input_dim, X, input_dim,
-				& beta, O, num_hidden));
+				& beta, Y, num_hidden));
+}
+
+template <>
+inline void FullyConnectedBWWeight < float >  (cublasHandle_t cublas_handle,
+	const float * const __restrict__ dY,
+	const float * const __restrict__  X,
+	      float * const __restrict__ dW,
+	const OpReqType grad_req, const unsigned batch_size,
+	const unsigned input_dim, const unsigned num_hidden)
+{
+	float alpha = 1.0, beta = float(grad_req == kAddTo);
+
+	CUBLAS_CALL(cublasSgemm(cublas_handle, // cuBLAS Handle
+	                        CUBLAS_OP_N, //  X
+				CUBLAS_OP_T, // dY^T
+				input_dim,   // dW.shape[1]
+				num_hidden,  // dW.shape[0]
+				batch_size,  //  X.shape[0]
+	                        &alpha,  X, input_dim, dY, num_hidden,
+				& beta, dW, input_dim));
+}
+
+template <>
+inline void FullyConnectedBWWeight < double > (cublasHandle_t cublas_handle,
+	const double * const __restrict__ dY,
+	const double * const __restrict__  X,
+	      double * const __restrict__ dW,
+	const OpReqType grad_req, const unsigned batch_size,
+	const unsigned input_dim, const unsigned num_hidden)
+{
+	double alpha = 1.0, beta = double(grad_req == kAddTo);
+
+	CUBLAS_CALL(cublasDgemm(cublas_handle, // cuBLAS Handle
+	                        CUBLAS_OP_N, //  X
+				CUBLAS_OP_T, // dY^T
+				input_dim,   // dW.shape[1]
+				num_hidden,  // dW.shape[0]
+				batch_size,  //  X.shape[0]
+	                        &alpha,  X, input_dim, dY, num_hidden,
+				& beta, dW, input_dim));
+}
+
+template <>
+inline void FullyConnectedBWData  (cublasHandle_t cublas_handle,
+	const float * const __restrict__ dY, 
+	const float * const __restrict__  W,
+	      float * const __restrict__ dX,
+	const OpReqType grad_req, const unsigned batch_size,
+	const unsigned input_dim, const unsigned num_hidden)
+{
+	float alpha = 1.0, beta = float(grad_req == kAddTo);
+
+	CUBLAS_CALL(cublasSgemm(cublas_handle, //cuBLAS Handle
+	                        CUBLAS_OP_N, //  W
+				CUBLAS_OP_N, // dY
+				input_dim,   // dX.shape[1]
+				batch_size,  // dX.shape[0]
+				num_hidden,  //  W.shape[0]
+				&alpha,  W, input_dim, dY, num_hidden,
+				& beta, dX, input_dim));
+}
+
+template <>
+inline void FullyConnectedBWData  (cublasHandle_t cublas_handle,
+	const double * const __restrict__ dY,
+	const double * const __restrict__  W, 
+	      double * const __restrict__ dX,
+	const OpReqType grad_req, const unsigned batch_size,
+	const unsigned input_dim, const unsigned num_hidden)
+{
+	double alpha = 1.0, beta = double(grad_req == kAddTo);
+
+	CUBLAS_CALL(cublasDgemm(cublas_handle, //cuBLAS Handle
+	                        CUBLAS_OP_N, //  W
+				CUBLAS_OP_N, // dY
+				input_dim,   // dX.shape[1]
+				batch_size,  // dX.shape[0]
+				num_hidden,  //  W.shape[0]
+				&alpha,  W, input_dim, dY, num_hidden,
+				& beta, dX, input_dim));
 }
 
 #endif // __CUDACC__
