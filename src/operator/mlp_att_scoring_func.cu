01cee1a9a9045e6f44bea6b362676c826ed2f8c2
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-18 21:59:34 -0500

    Removed double precious template instantiation.

--- src/operator/mlp_att_scoring_func.cu
+++ src/operator/mlp_att_scoring_func.cu
@@ -8,7 +8,7 @@ Operator * CreateOp < gpu > (MlpAttScoringFuncParam param, int dtype)
 {
 	Operator * op = nullptr;
 
-	MSHADOW_SGL_DBL_TYPE_SWITCH(dtype, DType, { op = new CUMlpAttScoringFuncOp < DType > (param); });
+	MSHADOW_SGL_DBL_TYPE_SWITCH(dtype, DType, { op = new CUMlpAttScoringFuncOp < float > (param); });
 
 	return op;
 }
