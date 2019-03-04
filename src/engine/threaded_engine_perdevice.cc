aa1892ad97c89bf24c35a6e6100c1c5823dc97a1
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-03-04 01:17:39 -0500

    Fixed the opr_name assignment from CPU_Worker to GPU_Worker.

--- src/engine/threaded_engine_perdevice.cc
+++ src/engine/threaded_engine_perdevice.cc
@@ -265,6 +265,11 @@ class ThreadedEnginePerDevice : public ThreadedEngine {
     OpenMP::Get()->on_start_worker_thread(false);
 
     while (task_queue->Pop(&opr_block)) {
+#if MXNET_USE_MEMORY_PROFILER
+      // @ArmageddonKnight CAUTION: Should we assignment the RunContext on each operator run?
+      run_ctx.ctx.name = opr_block->opr->opr_name == nullptr ? 
+          "" : opr_block->opr->opr_name;
+#endif // MXNET_USE_MEMORY_PROFILER
       this->ExecuteOprBlock(run_ctx, opr_block);
     }
     // Catch exception for CUDA driver shutdown
@@ -293,10 +298,6 @@ class ThreadedEnginePerDevice : public ThreadedEngine {
     OpenMP::Get()->on_start_worker_thread(true);
 
     while (task_queue->Pop(&opr_block)) {
-#if MXNET_USE_MEMORY_PROFILER
-      // @ArmageddonKnight CAUTION: Should we assignment the RunContext on each operator run?
-      run_ctx.ctx.name = opr_block->opr->opr_name;
-#endif // MXNET_USE_MEMORY_PROFILER
       this->ExecuteOprBlock(run_ctx, opr_block);
     }
   }
