2cc27b7e9cd7756f2a7719edd75dd9980d70efa7
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-03-05 16:52:00 -0500

    Changed back to the DMLC logging.

--- src/profiler/gpu_memory_profiler.cc
+++ src/profiler/gpu_memory_profiler.cc
@@ -2,25 +2,6 @@
 
 #include "./gpu_memory_profiler.h"
 
-inline std::string _StackTrace() {
-  using std::string;
-  std::ostringstream stacktrace_os;
-  const int MAX_STACK_SIZE = 32;
-  void *stack[MAX_STACK_SIZE];
-  int nframes = backtrace(stack, MAX_STACK_SIZE);
-  stacktrace_os << "Stack trace returned " << nframes << " entries:" << std::endl;
-  char **msgs = backtrace_symbols(stack, nframes);
-  if (msgs != nullptr) {
-    for (int frameno = 0; frameno < nframes; ++frameno) {
-      string msg = dmlc::Demangle(msgs[frameno]);
-      stacktrace_os << "[bt] (" << frameno << ") " << msg << "\n";
-    }
-  }
-  free(msgs);
-  string stack_trace = stacktrace_os.str();
-  return stack_trace;
-}
-
 #if MXNET_USE_MEMORY_PROFILER
 mxnet::profiler::GpuMemoryProfiler g_gpu_memory_profiler;
 
@@ -50,33 +31,21 @@ GpuMemoryProfiler::~GpuMemoryProfiler() { _fout.close(); _ferr.close(); }
 void GpuMemoryProfiler::addEntry(const std::string & tag,
                                  const std::size_t   alloc_size)
 {
-#define MB 1024 * 1024
-	_fout << tag << "," << alloc_size << std::endl;
+#define MB (1024.0 * 1024.0)
+	_fout << tag << "," << alloc_size / MB << std::endl;
 	std::cout << "[gpu_memory_profiler:info] " << 
 		"Allocating " << alloc_size / MB << " "
 		"with Context Tag " << tag << std::endl;
 
-	if (tag == "untagged")
+	if (tag == "untagged" || tag == "<unk>" || 
+	    tag == "_zeros"   || tag == "broadcast_lesser")
 	{
-		// unknown tags from the Python end
-		    _ferr << _StackTrace() << std::endl;
-		std::cerr << _StackTrace() << std::endl;
-	}
-	if (tag == "<unk>")
-	{
-		// unknown tags from the C++ end
-		    _ferr << _StackTrace() << std::endl;
-		std::cerr << _StackTrace() << std::endl;
-	}
-	if (tag == "_zeros")
-	{
-		    _ferr << _StackTrace() << std::endl;
-		std::cerr << _StackTrace() << std::endl;
-	}
-	if (tag == "broadcast_lesser")
-	{
-		    _ferr << _StackTrace() << std::endl;
-		std::cerr << _StackTrace() << std::endl;
+		// unknown tags from the Python end	
+		    _ferr << dmlc::StackTrace() << std::endl;
+		    _ferr << "[gpu_memory_profiler:info] " << 
+			"Allocating " << alloc_size / MB << " "
+			"with Context Tag " << tag << std::endl;
+		std::cerr << dmlc::StackTrace() << std::endl;
 	}
 #undef MB
 }
