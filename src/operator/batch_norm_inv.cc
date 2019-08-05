b508f30e09e4a8ccd1f82a14c2fc684c17a9d83d
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-08-04 20:12:17 -0400

    Switched to NNVM declaration.

--- src/operator/batch_norm_inv.cc
+++ src/operator/batch_norm_inv.cc
@@ -24,8 +24,84 @@ Operator * BatchNormInvProp::CreateOperatorEx(Context ctx,
 
 DMLC_REGISTER_PARAMETER(BatchNormInvParam);
 
-MXNET_REGISTER_OP_PROPERTY(BatchNormInv, BatchNormInvProp)
-	.describe("Applies the BatchNormInv operator.")
+bool BatchNormInvInferShape(const nnvm::NodeAttrs & attrs,
+	std::vector < TShape > *  in_shape,
+	std::vector < TShape > * out_shape)
+{
+	using namespace mshadow;
+
+	// output, mean, inv_var, gamma, beta
+	CHECK_EQ(in_shape->size(), 5); 
+
+	const TShape & oshape = (*in_shape)[int(EnumOpInputs::Output)];
+	
+	unsigned batch_size = oshape[1];
+
+	SHAPE_ASSIGN_CHECK(*in_shape, int(EnumOpInputs::Mean),   Shape1(batch_size));
+	SHAPE_ASSIGN_CHECK(*in_shape, int(EnumOpInputs::InvVar), Shape1(batch_size));
+	SHAPE_ASSIGN_CHECK(*in_shape, int(EnumOpInputs::Gamma),  Shape1(batch_size));
+	SHAPE_ASSIGN_CHECK(*in_shape, int(EnumOpInputs::Beta),   Shape1(batch_size));
+	
+	out_shape->clear();
+
+	out_shape->push_back((*in_shape)[int(EnumOpInputs::Output)]); // Data 
+
+	return true;
+}
+
+bool BatchNormInvInferType (const nnvm::NodeAttrs & attrs,
+	std::vector < int > *  in_type,
+	std::vector < int > * out_type)
+{
+	CHECK_GE(in_type->size(), 1U);
+
+	int itype = (*in_type)[0];
+
+	CHECK_NE(itype, -1) << "First input must have specified type.";
+
+	for (std::size_t i = 1; i < in_type->size(); ++i)
+	{
+		if ((*in_type)[i] == -1) 
+		{
+			(*in_type)[i] = itype;
+		}
+		else
+		{
+			CHECK_EQ((*in_type)[i], itype) << "This layer requires uniform type. " << 
+				"Expected " << itype << " v.s. given " << 
+				(*in_type)[i] << " at " << i;
+		}
+	}
+
+	out_type->clear();
+
+	out_type->push_back(itype); // Data
+	
+	return true;
+}
+
+// MXNET_REGISTER_OP_PROPERTY(BatchNormInv, BatchNormInvProp)
+NNVM_REGISTER_OP(BatchNormInv)
+	.set_num_inputs (5)
+	.set_num_outputs(1)
+	.set_attr_parser(ParamParser<BatchNormInvParam>)
+	.set_attr<nnvm::FInferShape>("FInferShape", BatchNormInvInferShape)
+  	.set_attr<nnvm::FInferType> ("FInferType",  BatchNormInvInferType)
+  	.set_attr<nnvm::FInplaceOption>("FInplaceOption",
+    		[](const NodeAttrs & attrs){
+      			return std::vector < std::pair < int, int > >{{0, 0}};
+    		})
+	.set_attr<nnvm::FListInputNames>("FListInputNames",
+  		[](const NodeAttrs& attrs) {
+    			return std::vector < std::string > 
+			    	{ "output", "mean", "inv_var", "gamma", "beta" };
+  		})
+	.set_attr<nnvm::FListInputNames>("FListOutputNames",
+  		[](const NodeAttrs& attrs) {
+    			return std::vector < std::string > { "data" };
+  		})
+	.set_attr<nnvm::FGradient>("FGradient", MakeZeroGradNodes)
+	.describe("Applies the BatchNorm inverse operator.")
 	.add_argument ("output",  "NDArray-or-Symbol", "Output")
 	.add_argument ("mean",    "NDArray-or-Symbol", "Mean")
 	.add_argument ("inv_var", "NDArray-or-Symbol", "Inverse Variance")
--- src/operator/batch_norm_inv.cu
+++ src/operator/batch_norm_inv.cu
@@ -13,5 +13,58 @@ Operator * CreateOp < gpu > (BatchNormInvParam param, int dtype)
 	return op;
 }
 
+void BatchNormInvComputeGPU(const nnvm::NodeAttrs & attrs,
+                            const OpContext & ctx,
+                            const std::vector < TBlob > &  in_data,
+                 	    const std::vector < OpReqType > &  req,
+                 	    const std::vector < TBlob > & out_data)
+{
+	using namespace mshadow;
+	typedef float DType;
+
+	std::size_t in_expected = 5, out_expected = 1;
+
+	CHECK_EQ( in_data.size(),  in_expected); // output, mean, inv_var, 
+						 // gamma, beta
+	CHECK_EQ(out_data.size(), out_expected); // data
+
+	Stream < gpu > * cuda_stream = ctx.get_stream < gpu > (); 
+
+	TBlob output  =  in_data[int(EnumOpInputs ::Output)];
+	TBlob mean    =  in_data[int(EnumOpInputs ::Mean)];
+	TBlob inv_var =  in_data[int(EnumOpInputs ::InvVar)];
+	TBlob gamma   =  in_data[int(EnumOpInputs ::Gamma)];
+	TBlob beta    =  in_data[int(EnumOpInputs ::Beta)];
+	TBlob data    = out_data[int(EnumOpOutputs::Data)];
+
+	CHECK_EQ(output .CheckContiguous(), true);
+	CHECK_EQ(mean   .CheckContiguous(), true);
+	CHECK_EQ(inv_var.CheckContiguous(), true);
+	CHECK_EQ(gamma  .CheckContiguous(), true);
+	CHECK_EQ(beta   .CheckContiguous(), true);
+	CHECK_EQ(data   .CheckContiguous(), true);
+
+	_cuda_batch_norm_inv_forward < DType >
+		<<<
+			(output.shape_.Size() - 1) / 128 + 1, 128, 0,
+			Stream < gpu > ::GetStream(cuda_stream)
+		>>>
+		(
+			reinterpret_cast < DType * > (output .dptr_),
+			reinterpret_cast < DType * > (mean   .dptr_),
+			reinterpret_cast < DType * > (inv_var.dptr_),
+			reinterpret_cast < DType * > (gamma  .dptr_),
+			reinterpret_cast < DType * > (beta   .dptr_),
+			reinterpret_cast < DType * > (data   .dptr_),
+			output.shape_.Size(),
+			output.shape_[1],
+			output.shape_.Size() / (output.shape_[0] * 
+				 		output.shape_[1])
+		);
+}
+
+NNVM_REGISTER_OP(BatchNormInv)
+	.set_attr<FCompute>("FCompute<gpu>", BatchNormInvComputeGPU);
+
 	} // namespace op
 } // namespace mxnet
