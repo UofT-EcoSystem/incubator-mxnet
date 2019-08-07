b550816111784f4d2e291d03587a08c68abd3504
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-08-07 15:35:44 -0400

    Integrated the warp-ctc plugin.

--- make/config.mk
+++ make/config.mk
@@ -182,8 +182,8 @@ USE_CPP_PACKAGE = 0
 # TORCH_PATH = $(HOME)/torch
 # MXNET_PLUGINS += plugin/torch/torch.mk
 
-# WARPCTC_PATH = $(HOME)/warp-ctc
-# MXNET_PLUGINS += plugin/warpctc/warpctc.mk
+WARPCTC_PATH = $(HOME)/warp-ctc
+MXNET_PLUGINS += plugin/warpctc/warpctc.mk
 
 # whether to use sframe integration. This requires build sframe
 # git@github.com:dato-code/SFrame.git
