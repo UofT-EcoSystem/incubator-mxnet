c7bd92f605f25f36cc9cf8fc71e4e95727ffe1c0
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-07-30 16:22:20 -0400

    Changed the make config file to enable CUDA and disable OpenCV.

--- make/config.mk
+++ make/config.mk
@@ -70,18 +70,23 @@ USE_TVM_OP = 0
 #---------------------------------------------
 
 # whether use CUDA during compile
-USE_CUDA = 0
+# <bojian/TVM-AutoDiff> Compile with CUDA enabled.
+# USE_CUDA = 0
+USE_CUDA = 1
 
 # add the path to CUDA library to link and compile flag
 # if you have already add them to environment variable, leave it as NONE
-# USE_CUDA_PATH = /usr/local/cuda
-USE_CUDA_PATH = NONE
+# <bojian/TVM-AutoDiff>
+USE_CUDA_PATH = /usr/local/cuda
+# USE_CUDA_PATH = NONE
 
 # whether to enable CUDA runtime compilation
 ENABLE_CUDA_RTC = 1
 
 # whether use CuDNN R3 library
-USE_CUDNN = 0
+# <bojian/TVM-AutoDiff>
+# USE_CUDNN = 0
+USE_CUDNN = 1
 
 # whether to use NVTX when profiling
 USE_NVTX = 0
@@ -94,7 +99,9 @@ USE_NCCL_PATH = NONE
 # whether use opencv during compilation
 # you can disable it, however, you will not able to use
 # imbin iterator
-USE_OPENCV = 1
+# <bojian/TVM-AutoDiff> Compile with OpenCV disabled.
+# USE_OPENCV = 1
+USE_OPENCV = 0
 # Add OpenCV include path, in which the directory `opencv2` exists
 USE_OPENCV_INC_PATH = NONE
 # Add OpenCV shared library path, in which the shared library exists
